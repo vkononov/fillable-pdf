f6bd5e604316b59f609315979f5081a92a8a80fe
Matheus Richard <matheusrichardt@gmail.com>
2023-07-12 11:30:54 -0300

    Move constants out of global namespace.
    Defining constants in the global namespace is dangerous as it might conflict with other libraries/apps.

--- lib/fillable-pdf/itext.rb
+++ lib/fillable-pdf/itext.rb
@@ -3,24 +3,26 @@ require 'rjb'
 
 Rjb.load(Dir.glob(File.expand_path('../../ext/*.jar', __dir__)).join(':'))
 
-module ITEXT
-  extend FillablePDF::SuppressWarnings
+class FillablePDF
+  module ITEXT
+    extend FillablePDF::SuppressWarnings
 
-  suppress_warnings do
-    ByteArrayOutputStream = Rjb.import 'com.itextpdf.io.source.ByteArrayOutputStream'
-    Canvas = Rjb.import 'com.itextpdf.layout.Canvas'
-    Div = Rjb.import 'com.itextpdf.layout.element.Div'
-    HorizontalAlignment = Rjb.import 'com.itextpdf.layout.properties.HorizontalAlignment'
-    Image = Rjb.import 'com.itextpdf.layout.element.Image'
-    ImageDataFactory = Rjb.import 'com.itextpdf.io.image.ImageDataFactory'
-    PdfAcroForm = Rjb.import 'com.itextpdf.forms.PdfAcroForm'
-    PdfDictionary = Rjb.import 'com.itextpdf.kernel.pdf.PdfDictionary'
-    PdfDocument = Rjb.import 'com.itextpdf.kernel.pdf.PdfDocument'
-    PdfFormXObject = Rjb.import 'com.itextpdf.kernel.pdf.xobject.PdfFormXObject'
-    PdfName = Rjb.import 'com.itextpdf.kernel.pdf.PdfName'
-    PdfReader = Rjb.import 'com.itextpdf.kernel.pdf.PdfReader'
-    PdfWriter = Rjb.import 'com.itextpdf.kernel.pdf.PdfWriter'
-    Rectangle = Rjb.import 'com.itextpdf.kernel.geom.Rectangle'
-    VerticalAlignment = Rjb.import 'com.itextpdf.layout.properties.VerticalAlignment'
+    suppress_warnings do
+      ByteArrayOutputStream = Rjb.import 'com.itextpdf.io.source.ByteArrayOutputStream'
+      Canvas = Rjb.import 'com.itextpdf.layout.Canvas'
+      Div = Rjb.import 'com.itextpdf.layout.element.Div'
+      HorizontalAlignment = Rjb.import 'com.itextpdf.layout.properties.HorizontalAlignment'
+      Image = Rjb.import 'com.itextpdf.layout.element.Image'
+      ImageDataFactory = Rjb.import 'com.itextpdf.io.image.ImageDataFactory'
+      PdfAcroForm = Rjb.import 'com.itextpdf.forms.PdfAcroForm'
+      PdfDictionary = Rjb.import 'com.itextpdf.kernel.pdf.PdfDictionary'
+      PdfDocument = Rjb.import 'com.itextpdf.kernel.pdf.PdfDocument'
+      PdfFormXObject = Rjb.import 'com.itextpdf.kernel.pdf.xobject.PdfFormXObject'
+      PdfName = Rjb.import 'com.itextpdf.kernel.pdf.PdfName'
+      PdfReader = Rjb.import 'com.itextpdf.kernel.pdf.PdfReader'
+      PdfWriter = Rjb.import 'com.itextpdf.kernel.pdf.PdfWriter'
+      Rectangle = Rjb.import 'com.itextpdf.kernel.geom.Rectangle'
+      VerticalAlignment = Rjb.import 'com.itextpdf.layout.properties.VerticalAlignment'
+    end
   end
 end
